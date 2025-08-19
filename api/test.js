45be95018759ef03ade9f4e664d9ecbbd03b37e9
Jesus Morales <21moralesjesus@gmail.com>
2025-08-19 16:35:32 -0400

    pruebaa

---
+++ api/test.js
@@ -0,0 +1,13 @@
+import { db } from '../../firebaseAdmin';
+
+export default async function handler(req, res) {
+  try {
+    const snapshot = await db.collection('businesses').limit(3).get();
+    const businesses = snapshot.docs.map(doc => doc.data());
+
+    res.status(200).json({ businesses });
+  } catch (error) {
+    console.error('Error en /test:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+}
