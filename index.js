74f82a01d19e514d45d065a7c8aa434938acd1ae
Jesus Morales <21moralesjesus@gmail.com>
2025-08-24 19:06:01 -0400

    eeee

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // index.js
+//import express from "express";
+//import cors from "cors";
+//import dotenv from "dotenv";
+//import nearbyRoutes from "./api/nearby.js"; // tu ruta nearby
+//dotenv.config();
+
+//const app = express();
+//app.use(express.json());
+//app.use(cors());
+
+//app.use("/api", nearbyRoutes); // monta /api/nearby
+
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => {
+//  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+//});
+// index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import nearbyRoutes from "./api/nearby.js"; // tu ruta nearby
+
 dotenv.config();
 
 const app = express();
@@ -11,7 +29,5 @@ app.use(cors());
 
 app.use("/api", nearbyRoutes); // monta /api/nearby
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
-});
+// 🚀 Exportar app para que Vercel lo use
+export default app;
